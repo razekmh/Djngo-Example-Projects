48c8f8e7060505ca58301ebe509c171388dc7406
Mahmoud <razekmh@gmail.com>
2020-02-26 01:23:08 +0200

    print submited variable in terminal

--- wordcount_proj/wordcount/views.py
+++ wordcount_proj/wordcount/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home (request):
-    return render(request, 'home.html', {'hithere': 'this is me'})
+    return render(request, 'home.html')
 
 def count (request):
+    fulltext = request.GET['fulltext']
+    print(fulltext)
     return render(request, 'count.html')
