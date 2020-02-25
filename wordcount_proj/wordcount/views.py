1b50685f147055db1b3e955fc20bf877cdec9b77
Mahmoud <razekmh@gmail.com>
2020-02-26 01:34:04 +0200

    for loop in the html

--- wordcount_proj/wordcount/views.py
+++ wordcount_proj/wordcount/views.py
@@ -7,4 +7,14 @@ def home (request):
 def count (request):
     fulltext = request.GET['fulltext']
 
-    return render(request, 'count.html', {'fulltext': fulltext})
+    wordlist = fulltext.split()
+
+    worddict = {}
+
+    for word in wordlist: 
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count' : len(wordlist), 'worddict': worddict.items()})
