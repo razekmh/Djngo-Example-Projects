a383d130ae7fe61ac540e264f654bd8d630fa80c
Mahmoud <razekmh@gmail.com>
2020-02-26 04:50:42 +0200

    empty homepage

--- portofolio_proj/blog/models.py
+++ portofolio_proj/blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField()
-    body = models.CharField(max_length=2000)
+    body = models.TextField(max_length=2000)
     image = models.ImageField(upload_to='images/')
