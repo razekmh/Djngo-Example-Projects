ef4944e84f6c3c22c323473891692187f284a6b4
Mahmoud <razekmh@gmail.com>
2020-02-26 04:28:51 +0200

    postgres db

--- portofolio_proj/portofolio/settings.py
+++ portofolio_proj/portofolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portofoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '9909',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
